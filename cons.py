1c2b052df487223a423a5df85c1abc7cb9c0e9df
YesicaCifuentes24 <yesica.cifuentes@utp.edu.co>
2017-11-21 16:50:08 -0500

    [ADD] Cons py file
    * Storage the constants used in the game.

---
+++ cons.py
@@ -0,0 +1,13 @@
+"""
+Constantes globales
+"""
+
+# Colores
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Dimension del juego
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
